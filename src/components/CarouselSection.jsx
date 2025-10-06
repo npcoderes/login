de1d21590f8d44201c2d078ecd4e9f9106f80cb6
npcoderes <nikulpanchal2505@gmail.com>
2025-10-06 16:53:52 +0530

    svg_add

--- src/components/CarouselSection.jsx
+++ src/components/CarouselSection.jsx
@@ -5,8 +5,8 @@ import img2 from "../assets/image2.webp";
 import img3 from "../assets/3.webp";
 import img4 from "../assets/4.webp";
 import img5 from "../assets/5.webp";
-import img6 from "../assets/1-Login.png"
-import img7 from "../assets/2-login.png"
+import img6 from "../assets/1-Login.svg"
+import img7 from "../assets/2-Login.svg"
 import "./CarouselSection.less";
 import { useMemo } from "react";
 
